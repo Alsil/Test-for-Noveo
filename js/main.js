327df634aa3b11fe5d502f6955c59b551d6705ab
Ilya Ivanov <alsirion@gmail.com>
2016-10-20 17:48:58 +0700

    add slider

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 (function ($) {
+    var mySwiper = new Swiper ('.swiper-container', {
+        loop: true,
+        pagination: '.swiper-pagination',
+        autoplay: 3000,
+        paginationClickable: true
+    })
 
 }(jQuery));
  
