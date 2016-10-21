4517c14b256df84d71ab88e37e7300bddb18b530
Ilya Ivanov <alsirion@gmail.com>
2016-10-21 14:42:20 +0700

    Add smooth scroll

--- js/main.js
+++ js/main.js
@@ -4,12 +4,27 @@
         pagination: '.swiper-pagination',
         autoplay: 5000,
         paginationClickable: true
-    })
-}(jQuery));
-(function ($){
+    });
     $('#hamburger').click(function(){
         $('#hamburger, .menu').toggleClass("open");
-    })
+    });
+    $("#menu").on("click", "a", function (event) {
+        var id = $(this).attr('href'),
+            top = $(id).length ? $(id).offset().top : 0;
+        $('body').animate({scrollTop: top}, 300);
+    });
+    $('#scroll-to-top').click( function () {
+        $('body').animate({scrollTop: 0}, 300);
+    });
+    $(window).scroll(function(){
+        if($(window).scrollTop() > 100) {
+            $('#scroll-to-top').fadeIn();
+        }
+        else {
+            $('#scroll-to-top').fadeOut();
+        }
+    });
 }(jQuery));
+
  
 
