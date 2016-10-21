f63a278a49355ee38051d991e08cc9223c9fc984
Ilya Ivanov <alsirion@gmail.com>
2016-10-21 16:33:11 +0700

    some bug fixes in js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 (function ($) {
+    var $scrolTopBtn = $('#scroll-to-top');
     var mySwiper = new Swiper ('.swiper-container', {
         loop: true,
         pagination: '.swiper-pagination',
@@ -6,24 +7,21 @@
         paginationClickable: true
     });
     $('#hamburger').click(function(){
-        $('#hamburger, .menu').toggleClass("open");
+        $('#hamburger, #menu').toggleClass("open");
     });
     $("#menu").on("click", "a", function (event) {
         var id = $(this).attr('href'),
             top = $(id).length ? $(id).offset().top : 0;
         $('body').animate({scrollTop: top}, 300);
     });
-    $('#scroll-to-top').click( function () {
-        $('body').animate({scrollTop: 0}, 300);
+    $scrolTopBtn.click( function () {
+        $('body').animate({scrollTop: 0}, 300, function() {
+            $scrolTopBtn.hide()
+        });
+
     });
     $(window).scroll(function(){
-        if($(window).scrollTop() > 100) {
-            $('#scroll-to-top').fadeIn();
-        }
-        else {
-            $('#scroll-to-top').fadeOut();
-        }
-    });
+        $(window).scrollTop() > 100 ? $scrolTopBtn.stop().fadeIn() : $scrolTopBtn.stop().fadeOut()});
 }(jQuery));
 
  
