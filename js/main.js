9e5e3429e53d3ce5780f946e367082a68430758a
Ilya Ivanov <alsirion@gmail.com>
2016-10-22 15:11:35 +0700

    add another fonts + js bug fixes + css rework

--- js/main.js
+++ js/main.js
@@ -1,28 +1,35 @@
 (function ($) {
-    var $scrolTopBtn = $('#scroll-to-top');
-    var mySwiper = new Swiper ('.swiper-container', {
+    var $scrolTopBtn = $('#scroll-to-top'),
+        $window = $(window),
+        $body = $('body');
+    var mySwiper = new Swiper('.swiper-container', {
         loop: true,
         pagination: '.swiper-pagination',
         autoplay: 5000,
         paginationClickable: true
     });
+
     $('#hamburger').click(function(){
         $('#hamburger, #menu').toggleClass("open");
     });
+
     $("#menu").on("click", "a", function (event) {
         var id = $(this).attr('href'),
             top = $(id).length ? $(id).offset().top : 0;
-        $('body').animate({scrollTop: top}, 300);
+        $body.animate({scrollTop: top}, 300);
     });
+
     $scrolTopBtn.click( function () {
-        $('body').animate({scrollTop: 0}, 300, function() {
+        $body.animate({scrollTop: 0}, 300, function() {
             $scrolTopBtn.hide()
         });
-
     });
-    $(window).scroll(function(){
-        $(window).scrollTop() > 100 ? $scrolTopBtn.stop().fadeIn() : $scrolTopBtn.stop().fadeOut()});
-}(jQuery));
+
+    $window.scroll(function(){
+        $window.scrollTop() > 100 ? $scrolTopBtn.stop().fadeIn() : $scrolTopBtn.stop().fadeOut()
+    });
+
+})(jQuery);
 
  
 
